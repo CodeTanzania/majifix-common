18226abdda530f8967a61efc6ee8c3fa2d9dffde
Benson Maruchu <benmaruchu@gmail.com>
2018-05-16 16:01:22 +0300

    add env properties which can't not be set by a user

--- test/default.env.spec.js
+++ test/default.env.spec.js
@@ -1,12 +1,28 @@
 'use strict';
 
+/* dependencies */
+const path = require('path');
+const { expect } = require('chai');
+
+/* declarations */
+const env = require(path.join(__dirname, '..', 'lib', 'env'));
+
+
 describe('default', function () {
 
-    describe('env', function () {
-        expect(env.API_VERSION).to.exist;
-        expect(env.DEFAULT_LANGUAGE).to.exist;
+  describe('env', function () {
+    it('should have default api version', function () {
+      expect(env.API_VERSION).to.exist;
     });
 
+    it('should have default locale', function () {
+      expect(env.DEFAULT_LOCALE).to.exist;
+    });
+
+    it('should have default calling code', function () {
+      expect(env.DEFAULT_CALLING_CODE).to.exist;
+    });
+  });
 });
 
 
--- test/parsed.env.spec.js
+++ test/parsed.env.spec.js
@@ -1,15 +1,33 @@
 'use strict';
 
-process.env.API_VERSION = '1.0.0';
-process.env.DEFAULT_LOCALE = 'en';
+/* dependencies */
+const path = require('path');
+const { expect } = require('chai');
+
+const API_VERSION = process.env.API_VERSION = '1.0.1';
+const DEFAULT_LOCALE = process.env.DEFAULT_LOCALE = 'sw';
+const DEFAULT_CALLING_CODE = process.env.DEFAULT_CALLING_CODE = '254';
+
+/* declarations */
+const env = require(path.join(__dirname, '..', 'lib', 'env'));
 
 describe('parsed', function () {
 
-    describe('env', function () {
-        expect(env.API_VERSION).to.exist;
-        expect(env.DEFAULT_LANGUAGE).to.exist;
+  describe('env', function () {
+    it('should use default api version', function () {
+      expect(env.API_VERSION).to.exist;
+      expect(env.API_VERSION).to.be.equal(API_VERSION);
     });
 
+    it('should use default locale value set by a user', function () {
+      expect(env.DEFAULT_LOCALE).to.exist;
+      expect(env.DEFAULT_LOCALE).to.be.equal(DEFAULT_LOCALE);
+    });
+
+    it('should use default calling code value set by user', function () {
+      expect(env.DEFAULT_CALLING_CODE).to.exist;
+      expect(env.DEFAULT_CALLING_CODE).to.be.equal(DEFAULT_CALLING_CODE);
+    });
+  });
+
 });
-
-
