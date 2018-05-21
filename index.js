0173d6a40e814c129d88cf5844d1578e9ddbc648
Benson Maruchu <benmaruchu@gmail.com>
2018-05-21 17:16:30 +0300

    Expose schema commons - write spec for schema commons - improve spec for env commons

--- index.js
+++ index.js
@@ -3,5 +3,19 @@
 /* dependencies */
 const path = require('path');
 
+/* declarations */
+const env = require(path.join(__dirname, 'lib', 'env'));
+const schema = require(path.join(__dirname, 'lib', 'schema'));
 
-exports.env = require(path.join(__dirname, 'lib', 'env'));
+Object.defineProperties(exports, {
+  env: {
+    get: function () {
+      return env;
+    }
+  },
+  schema: {
+    get: function () {
+      return schema;
+    }
+  }
+});
--- test/default.env.spec.js
+++ test/default.env.spec.js
@@ -11,19 +11,19 @@ const env = require(path.join(__dirname, '..', 'lib', 'env'));
 describe('default', function () {
 
   describe('env', function () {
-    it('should have default api version', function () {
+    it('should expose default api version', function () {
       expect(env.API_VERSION).to.exist;
     });
 
-    it('should have default locale', function () {
+    it('should expose default locale', function () {
       expect(env.DEFAULT_LOCALE).to.exist;
     });
 
-    it('should have default calling code', function () {
+    it('should expose default calling code', function () {
       expect(env.DEFAULT_CALLING_CODE).to.exist;
     });
 
-    it('should have locales', function () {
+    it('should expose locales', function () {
       expect(env.LOCALES).to.exist;
       expect(env.LOCALES).to.be.an('array');
     });
