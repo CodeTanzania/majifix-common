463fecdd1092d0ffe54b866d1f283d7558608f22
rijkerd <richardaggrey7@gmail.com>
2019-04-16 20:20:33 +0300

    refactor: use arrow function on test

--- test/phone.spec.js
+++ test/phone.spec.js
@@ -10,30 +10,30 @@ const { expect } = require('chai');
 const phone = require(path.join(__dirname, '..', 'lib', 'phone'));
 const common = require(path.join(__dirname, '..'));
 
-describe('phone', function () {
+describe('phone', () => {
 
-  before(function () {
+  before(() => {
     process.env.DEFAULT_COUNTRY_CODE = 'TZ';
   });
 
-  it('should expose toE164 formatter', function () {
+  it('should expose toE164 formatter', () => {
     expect(phone.toE164).to.exist;
     expect(phone.toE164).to.be.a('function');
   });
 
-  it('should use default country code', function () {
+  it('should use default country code', () => {
     const phoneNumber = phone.toE164('0714969698');
     expect(phoneNumber).to.exist;
     expect(phoneNumber).to.be.eql('255714969698');
   });
 
-  it('should use country code param', function () {
+  it('should use country code param', () => {
     const phoneNumber = phone.toE164('0714969698', 'KE');
     expect(phoneNumber).to.exist;
     expect(phoneNumber).to.be.eql('254714969698');
   });
 
-  it('should be exported', function () {
+  it('should be exported', () => {
     expect(common).to.exist;
     expect(common.phone).to.exist;
     expect(common.phone).to.be.eql(phone);
